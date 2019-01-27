40a043c5392948b9efeb90dfdfcafd48590210e9
Ronin Cunningham <ronincunningham1@gmail.com>
2019-01-27 08:05:08 -0800

    Finished import camera framework

--- Inbox-TechJobs/ProfileViewController.swift
+++ Inbox-TechJobs/ProfileViewController.swift
@@ -8,13 +8,38 @@
 
 import UIKit
 
-class ProfileViewController : UIViewController {
+class ProfileViewController : UIViewController, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
     @IBOutlet var profilePicture: UIImageView!
+    @IBOutlet weak var userImage: UIImageView!
+    
     @IBAction func importImage(_ sender: Any) {
+        let image = UIImagePickerController()
+        image.delegate = self
         
+        image.sourceType = UIImagePickerController.SourceType.photoLibrary
+        image.allowsEditing = false
+        self.present(image, animated: true)
     }
     
-    @IBOutlet weak var userImage: UIImageView!
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        if let image = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
+            userImage.image = image
+            //PROFILE IMAGE GLOBALLY CHANGES, AND IN PROFILE TAB, AND IN INBOX TAB
+            // UPLOAD IMAGE TO DATABASE???
+        } else {
+            //Error message
+        }
+        
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+    
+    
+    
+    
+    
+    
+    
     override func viewDidLoad() {
         profilePicture.image = UIImage(imageLiteralResourceName: "icons8-name-90")
     }
